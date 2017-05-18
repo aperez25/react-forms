75f9515761a71cc0e054c661e6767f6924f51d3c
CindySchalit <cindyschalit@gmail.com>
2017-05-17 18:14:53 -0700

    trying to get artist filter to render

---
+++ browser/react/components/FilterableArtistsContainer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import FilterInput from './FilterInput';
+import Artists from './Artists';
+
+export default class FilterableArtistsContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: ''
+    }
+  }
+
+  handleChange (evt) {
+    const value = evt.target.value;
+    this.setState({
+      inputValue: value
+    });
+  }
+
+  render () {
+    const inputValue = this.state.inputValue;
+    const filteredArtists = this.props.artists.filter(artist =>
+      artist.name.match(inputValue));
+    return (
+      <div>
+        <FilterInput handleChange={this.handleChange} />
+        <Artists artists={filteredArtists} />
+      </div>
+    );
+}
+}
